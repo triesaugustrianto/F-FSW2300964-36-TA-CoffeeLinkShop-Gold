6ba333edeb039bdadaa6234eb56132f3090719c3
Tries Augustrianto <triesatrianto@gmail.com>
2023-08-09 10:26:55 +0700

    add json file and get json

--- routers/routerProducts.js
+++ routers/routerProducts.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const router = require('express').Router()
-app.use(express.urlencoded())
+app.use(express.urlencoded()) // untuk post method mengambil req body
 const path = require('path')
+//const {json} = require('body-parser')
+
+const products = require('../db/products.json')
+
 const logger = (req, res, next) => {
     console.log(`Router level : ${req.method} ${req.url}`)
     next()
@@ -10,7 +14,14 @@ const logger = (req, res, next) => {
 router.use(logger);
 
 router.get('/', (req, res) => {
-    res.render('products')
+    res.json(products)
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    res.json(products.find((product) => {
+        return product.id == id
+    }))
 })
 
 module.exports = router
